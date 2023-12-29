c9a8186fdc8eb99e773f5627b9395eebb68fc9fc
kmgbarroga <100612482+kmgbarroga@users.noreply.github.com>
2023-12-29 17:06:50 +0800

    'clear-up'

--- storage/framework/views/2ec95e80698f308b0a1849d9268bf5faa7671456.php
+++
@@ -1,7 +0,0 @@
-<script>
-    var statusUpdateURL = "<?php echo e(route('change.status')); ?>";
-    var token = "<?php echo e(Session::token()); ?>";
-    var convertURL = "<?php echo e(route('convert.account')); ?>";
-    var subUser = "<?php echo e(getSubUser(Auth::user()->type)); ?>";
-</script>
-<?php /**PATH /home/u428933156/domains/swcslasher.online/public_html/dmiphonl_source/resources/views/partials/_jsvariables.blade.php ENDPATH**/ ?>
